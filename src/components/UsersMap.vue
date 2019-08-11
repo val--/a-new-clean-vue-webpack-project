1bab2d0bf5242755114010b6a07978375d9761a0
val-- <khayvav@gmail.com>
2019-08-11 14:39:25 +0200

    adding map component

---
+++ src/components/UsersMap.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<l-map :zoom="zoom" :center="center" style="height: 750px; width: 900px">
+			<l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+			<l-marker :lat-lng="marker"></l-marker>
+		</l-map>
+	</div>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+export default {
+    name: 'UsersMap',
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker
+    },
+    data: function(){
+		return {
+			zoom:13,
+			center: L.latLng(47.218349,  -1.539886),
+			url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+			attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+			marker: L.latLng(47.218349,  -1.539886),
+		}
+	}
+}
+</script>
+
+<style>
+html, body, #app {
+	height: 100%;
+	margin: 0;
+}
+</style>
