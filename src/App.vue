9606e21017e53672d69300b47a18874dddbf960d
val-- <khayvav@gmail.com>
2019-08-11 12:40:44 +0200

    clean vue / webpack project : first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="World" />
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
+
+<style>
+/*body{background-color:red;}*/
+</style>
